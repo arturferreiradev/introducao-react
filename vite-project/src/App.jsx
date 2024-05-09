85a1266fc09da1a67bb13b036b53c5e03ed54c41
Artur Ferreira <ferreira.artur.dev@gmail.com>
2024-05-08 22:02:06 -0300

    Desafio incluir rodape

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,17 +1,18 @@
-import { Cabecalho, Conteudo } from "./components";
+import { Cabecalho, Conteudo, Rodape } from "./components";
 
 import "./App.css";
 
 const App = () => {
   return (
-    <> {/* fragment */}
-      <Cabecalho nomeUsuario="Joana"/>
+    <> 
+      <Cabecalho nomeUsuario="Joana" />
       <Conteudo>
         <h1>Titulo</h1>
         <p>Texto texto texto</p>
       </Conteudo>
+      <Rodape criador="Artur"/>      
     </>
   );
 };
 
-export { App } ;
+export { App };
